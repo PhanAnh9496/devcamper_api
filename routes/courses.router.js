7c5775677bede01d9f4d3bcc4a8d34f73cd49bec
PhanAnh9496 <phananh9496@gmail.com>
2022-03-08 15:33:35 +0700

    api: update and delete course

--- routes/courses.router.js
+++ routes/courses.router.js
@@ -3,10 +3,12 @@ const {
   getCourses,
   getCourse,
   addCourse,
+  updateCourse,
+  deleteCourse
 } = require('../controllers/courses.controller');
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses).post(addCourse);
-router.route('/:id').get(getCourse);
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
