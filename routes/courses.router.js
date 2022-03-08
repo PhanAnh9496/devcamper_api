0ecbd3d16abca31185b382e361e111531f711992
PhanAnh9496 <phananh9496@gmail.com>
2022-03-08 10:13:42 +0700

    api: get single course

--- routes/courses.router.js
+++ routes/courses.router.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getCourses } = require('../controllers/courses.controller');
+const { getCourses, getCourse } = require('../controllers/courses.controller');
 const router = express.Router({ mergeParams: true });
 
 router.route('/').get(getCourses);
+router.route('/:id').get(getCourse);
 
 module.exports = router;
