a331f51188236f5cfcd3807e906f881fa0118747
PhanAnh9496 <phananh9496@gmail.com>
2022-03-09 09:36:28 +0700

    refactoring: pagination and query string into general middleware

--- routes/bootcamps.router.js
+++ routes/bootcamps.router.js
@@ -9,6 +9,10 @@ const {
   bootcampPhotoUpload,
 } = require('../controllers/bootcamps.controller');
 
+const Bootcamp = require('../models/Bootcamp');
+
+const advancedResults = require('../middleware/advancedResults');
+
 // Include other resource routers
 const courseRouter = require('./courses.router');
 
@@ -17,7 +21,7 @@ const router = express.Router();
 // Re-route into other resource routers
 router.use('/:bootcampId/courses', courseRouter);
 
-router.route('/').get(getBootcamps).post(createBootcamp);
+router.route('/').get(advancedResults(Bootcamp, 'courses'),getBootcamps).post(createBootcamp);
 
 router.route('/:id/photo').put(bootcampPhotoUpload);
 
