ec92d650490ade326fd263704244c2a05ae0057d
PhanAnh9496 <phananh9496@gmail.com>
2022-03-10 09:55:17 +0700

    jwt sign and login api

--- server.js
+++ server.js
@@ -2,15 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
 const fileupload = require('express-fileupload');
+const cookieParser = require('cookie-parser')
+const morgan = require('morgan');
+const errorHandler = require('./middleware/error');
+const connectDB = require('./config/db');
+
 //load env
 dotenv.config({ path: './config/config.env' });
 const app = express();
 const bootcampsRouter = require('./routes/bootcamps.router');
 const coursesRouter = require('./routes/courses.router');
 const authRouter = require('./routes/auth.router');
-const morgan = require('morgan');
-const errorHandler = require('./middleware/error');
-const connectDB = require('./config/db');
 
 const PORT = process.env.PORT || 5000;
 
@@ -20,6 +22,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+//ConnectDB
+connectDB();
 
 //File uploading
 app.use(fileupload());
@@ -27,8 +31,6 @@ app.use(fileupload());
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//ConnectDB
-connectDB();
 
 //Route files
 app.use('/api/v1/bootcamps', bootcampsRouter);
